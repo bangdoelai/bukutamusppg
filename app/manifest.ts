adbecd1f7a9e34be9b66d6dff9118399be5cab25
Abdullah <bangdoelai@gmail.com>
2026-08-20 11:07:26 +0800

    Set foundation logo as PWA icon and favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,6 +12,8 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: 'id',
     icons: [
       { src: '/icon-light-32x32.png', sizes: '32x32', type: 'image/png' },
+      { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+      { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
       { src: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   }
